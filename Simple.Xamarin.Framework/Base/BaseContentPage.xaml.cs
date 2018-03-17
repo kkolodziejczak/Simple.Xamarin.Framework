fcab9014ba548a54796e17984a9eb6a7a3ba130d
Krzysztof Kołodziejczak <mrthemo@gmail.com>
2018-03-17 19:28:29 +0100

    Add Base view classes and components

---
+++ Simple.Xamarin.Framework/Base/BaseContentPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Simple.Xamarin.Framework.Base
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BaseContentPage : ContentPage
+    {
+        public BaseContentPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            if (BindingContext is BasePageViewModel vm)
+            {
+                return vm.OnBackButtonPressed();
+            }
+            // else pop back
+            return base.OnBackButtonPressed();
+        }
+    }
+}
---
+++ Simple.Xamarin.Framework/Converters/NegateBoolValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Simple.Xamarin.Framework
+{
+    public class NegateBoolValueConverter : BaseValueConverter<NegateBoolValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || !(value is bool boolValue))
+                return null;
+
+            return !boolValue;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
